168517659cbbcdb306b01f245e62da184ce2210d
wdbrow11 <will.brown.hc@gmail.com>
2018-08-07 22:10:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+function getUKTime(){
+  let UKTime = new Date().getHours() + 5;
+  return UKTime
+}
+
+function discountTicket(price){
+  let discountPrice = price * 0.80;
+  return discountPrice;
+}
+
+function tweetReply(fanName){
+  let greeting = (`Hey ${fanName}, thanks so much for the support! I really appreciate it!`);
+  return greeting;
+}
